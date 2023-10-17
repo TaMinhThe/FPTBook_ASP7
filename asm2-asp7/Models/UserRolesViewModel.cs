236089a20f19979c018e59e0df77646b94c0f392
minhvqgcc210229 <121932119+minhvqgcc210229@users.noreply.github.com>
2023-10-17 09:52:05 +0700

    Delete some controller no Need

--- asm2-asp7/Models/UserRolesViewModel.cs
+++ asm2-asp7/Models/UserRolesViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace asm2_asp7.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace asm2_asp7.Models
 {
     public class UserRolesViewModel
     {
+        [Key]
         public string UserId { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public IEnumerable<string> Roles { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public IEnumerable<string>? Roles { get; set; }
     }
 }
